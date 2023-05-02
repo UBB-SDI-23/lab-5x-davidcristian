dca2c8569e5d067a73da95fb38b89f3fa6c6d81a
davidcristian <122480408+davidcristian@users.noreply.github.com>
2023-05-02 04:31:04 +0300

    SHP-111 Add User and UserProfile

--- StoreAPI/StoreAPI/Models/StoreShift.cs
+++ StoreAPI/StoreAPI/Models/StoreShift.cs
@@ -11,5 +11,8 @@
         // Hidden from the API because it's not in the DTO
         public virtual Store Store { get; set; } = null!;
         public virtual StoreEmployee StoreEmployee { get; set; } = null!;
+
+        public virtual long? UserId { get; set; }
+        public virtual User User { get; set; } = null!;
     }
 }
