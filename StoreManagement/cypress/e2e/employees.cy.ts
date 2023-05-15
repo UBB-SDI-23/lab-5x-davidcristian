aae28d410e26e22e533fff026567bfb5e9488684
davidcristian <122480408+davidcristian@users.noreply.github.com>
2023-05-16 01:47:07 +0300

    SHP-120 Add E2E tests

---
+++ StoreManagement/cypress/e2e/employees.cy.ts
@@ -0,0 +1,14 @@
+describe("Employees", () => {
+    describe("on opening the list of employees", () => {
+        it("should show the employees in the database", () => {
+            cy.intercept("GET", "**/api/storeemployees/**", {
+                fixture: "employees.json",
+            });
+
+            cy.visit("http://localhost:5173/employees");
+            cy.get('[data-testid="test-all-employees-container"]').should(
+                "exist"
+            );
+        });
+    });
+});
